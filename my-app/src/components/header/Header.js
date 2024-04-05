d1458c954f811bd89e25b5a59123e29f4958edee
v1adster <v1adsterpro@yandex.ru>
2024-04-05 14:15:00 +0300

    app-changes

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,8 @@
+import styles from './Header.module.scss';
+const Header=()=>{
+    return (<div>
+        <header className={styles.header}>Hello</header>
+        <p>j;kasd;as</p>
+    </div>)
+}
+export default Header;
